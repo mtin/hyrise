d8d1bb91540c95ed4c7e5aac3081d861192d3df4
David Schwalb <schwalb.david@gmail.com>
2013-10-22 14:17:59 +0200

    Adds all input from Commit operator to the output

--- src/lib/access/tx/Commit.cpp
+++ src/lib/access/tx/Commit.cpp
@@ -11,6 +11,10 @@ namespace {
 
 void Commit::executePlanOperation() {
   tx::TransactionManager::commitTransaction(_txContext);
+  
+  // add all input data to the output
+  for (size_t i=0; i<input.numberOfTables(); ++i)
+  	addResult(input.getTable(0));
 }
 
 std::shared_ptr<PlanOperation> Commit::parse(Json::Value &data) {
